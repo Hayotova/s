12a480cdcc890efafdb62f5b59bf79f5cb68a679
KHayotova1 <sitorahayotova7455@mail.ru>
2024-07-04 09:18:34 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        400:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+$('.owl-carousel-two').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        400:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
